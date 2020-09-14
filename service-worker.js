e38ec5ac6e234148e47ca2360945287a68c13305
Keonwoo Kim <keonwoo@kaist.ac.kr>
2020-09-14 11:45:13 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c65fd4dd554800faa2751da00dcff0f1.js"
+  "/precache-manifest.7d2b312ac4654bf18f46650e6f99869c.js"
 );
 
 self.addEventListener('message', (event) => {
